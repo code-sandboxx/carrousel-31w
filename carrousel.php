812f25b72234c8d738c2596405689d9a18ae5098
Olena K <234inbox@gmail.com>
2023-03-08 10:51:43 -0500

    C11 Début du carrousel

---
+++ carrousel.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Extension carrousel permet d'afficher dans une boite modale les images d'une galerie
+ * Package name: Carrousel
+ * Version: 1.0.0
+ */
+/*
+Plugin name: Carrousel
+Author: Olena
+Plugin URI: https://github.com/code-sandboxx/31w-theme-wordpress/tree/carrousel
+Description: Permet d'afficher dans une boite modale les images d'une galerie avec un système de navigation
+*/
