11d02e97bcf21344d93a194ef0e11aa860d39c41
Olena K <234inbox@gmail.com>
2023-03-09 14:51:40 -0500

    C11 carrousel.php enqueue et génération du code HTML

--- carrousel.php
+++ carrousel.php
@@ -1,13 +1,44 @@
 <?php
 
 /**
- * Extension carrousel permet d'afficher dans une boite modale les images d'une galerie
+ * Extension carrousel permet d'afficher dans une boite modale les images d'une galerie 
  * Package name: Carrousel
  * Version: 1.0.0
  */
 /*
 Plugin name: Carrousel
 Author: Olena
-Plugin URI: https://github.com/code-sandboxx/31w-theme-wordpress/tree/carrousel
+Plugin URI: https://github.com/code-sandboxx/carrousel-31w/tree/tp1
 Description: Permet d'afficher dans une boite modale les images d'une galerie avec un système de navigation
 */
+
+function carrousel_enqueue(){
+
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style('em_plugin_carrousel_css',
+                    plugin_dir_url(__FILE__) . "style.css", // __FILE__ - fichier qui est en train d’être exécuté
+                    array(),
+                    $version_css);
+
+    wp_enqueue_script('em_plugin_carrousel_js',
+                    plugin_dir_url(__FILE__) ."js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);
+}
+
+add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+
+function creation_carrousel(){
+
+    return '<button class="bouton__ouvrir">Ouvrir</button>
+        <div class="carrousel">
+        <button class="bouton__x">X</button>
+        <figure class="carrousel__figure"></figure>
+        <form class="carrousel__form"></form>
+        </div>';
+}    
+
+add_shortcode('carrousel', 'creation_carrousel'); // ajouter [carrousel] dans le texte d'article Ma gallerie accueil
